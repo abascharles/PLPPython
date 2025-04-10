c1d0069142c724ec167bdf4a1d7e0553b63bb02e
abascharles <abascharlesbenard@gmail.com>
2025-04-10 20:32:08 +0300

    Printing the random number

--- Week1/guessNumber.py
+++ Week1/guessNumber.py
@@ -1,5 +1,6 @@
 import random
 secret_number = random.randint(1, 100)
+print(secret_number)
 
 guess = int(input("Guess a number (1-100): "))
 
@@ -10,3 +11,17 @@ elif guess < secret_number:
     print("Too High")
 else:
     print("Correct")
+
+attempts = 0
+
+while True:
+    guess = int(input("Guess a number (1-100): "))
+    attempts += 1
+
+    if guess < secret_number:
+        print("Too Low")
+    elif guess > secret_number: 
+        print("Too High")
+    else:
+        print("Correct! Attempts: ", attempts)
+        break
