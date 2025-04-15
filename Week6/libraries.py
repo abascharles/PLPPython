4945e870628beb7bb16f701b297b4312b7b98174
abascharles <abascharlesbenard@gmail.com>
2025-04-15 11:02:57 +0300

    Libraries

--- Week6/libraries.py
+++ Week6/libraries.py
@@ -1,1 +1,11 @@
-# Pythin libraries - prewritten code 
+# Pythin libraries - prewritten code that you can use
+import math
+import random
+from math import sqrt
+print(math.sqrt(17)) # 4.0
+print(math.pow(2, 3)) # 8.0
+
+
+# Print a Random Number
+
+print("Random Number: ", random.randint(1, 12)) # Random number between 1 and 100
